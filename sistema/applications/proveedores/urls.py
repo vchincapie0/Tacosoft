87e3975ea88d17e260aff25041f84feb1ab9239c
vivian_pc <carohinca1997@gmail.com>
2024-02-16 11:33:33 -0500

    Creacion url para delete_proveedores

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('list_proveedores/',views.ProveedoresListView.as_view(),name='list_proveedores'),
     path('add_proveedor/',views.ProveedoresCreateView.as_view(),name='add_proveedores'),
     path('edit_proveedor/<pk>',views.ProveedorUpdateView.as_view(),name='update_proveedores'),
+    path('delete_proveedor/<pk>',views.ProveedoresDeleteView.as_view(),name='delete_proveedores'),
+
 
 
 ]
