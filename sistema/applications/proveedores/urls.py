cae659264b788e2af72bec46ec6210fdb5e31de5
vivian_pc <carohinca1997@gmail.com>
2024-02-16 10:25:16 -0500

    cambio de name_app en proveedores/urls

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name='mp_app'
+app_name='proveedores_app'
 
 urlpatterns = [
     path('list_proveedores/',views.ProveedoresListView.as_view(),name='list_proveedores')
