2dae6511641320f116eae63bcc3fa8120f437857
vivian_pc <carohinca1997@gmail.com>
2024-02-16 10:42:09 -0500

    Se añade url add_proveedor en urls.py

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='proveedores_app'
 
 urlpatterns = [
-    path('list_proveedores/',views.ProveedoresListView.as_view(),name='list_proveedores')
+    path('list_proveedores/',views.ProveedoresListView.as_view(),name='list_proveedores'),
+    path('add_proveedor/',views.ProveedoresCreateView.as_view(),name='add_proveedores'),
+
 ]
