b1a96ab51eda3b53574b6aa73a502cb2e86e616d
vivian_pc <carohinca1997@gmail.com>
2024-02-16 11:01:21 -0500

    fix error in url

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -6,7 +6,7 @@ app_name='proveedores_app'
 urlpatterns = [
     path('list_proveedores/',views.ProveedoresListView.as_view(),name='list_proveedores'),
     path('add_proveedor/',views.ProveedoresCreateView.as_view(),name='add_proveedores'),
-    path('edit_proveedor/',views.ProveedorUpdateView.as_view(),name='update_proveedores'),
+    path('edit_proveedor/<pk>',views.ProveedorUpdateView.as_view(),name='update_proveedores'),
 
 
 ]
