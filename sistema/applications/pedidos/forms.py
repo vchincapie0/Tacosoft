d8bb6ddf34efa88ea308f0ad89db4721eaca265e
vivian_pc <carohinca1997@gmail.com>
2024-02-20 15:56:04 -0500

    creacion de field en clase pedidoscreateform

--- sistema/applications/pedidos/forms.py
+++ sistema/applications/pedidos/forms.py
@@ -6,4 +6,13 @@ class PedidosCreateForm(forms.ModelForm):
     class Meta:
 
         model=Pedidos
-        
+        fields=(
+            'ref_pedido',
+            'pedi_user',
+            'pedi_fecha',
+            'pedi_estado',
+            'pedi_comprobatePago',
+            'pedi_proveedor',
+            'pedi_materiaprima',
+            'pedi_insumos',
+        )
