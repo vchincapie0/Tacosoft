c58111f9eb0a653c428dd030f607978c4bcab827
vivian_pc <carohinca1997@gmail.com>
2024-02-22 16:08:31 -0500

    creacion widgets de forms.py en pedidos

--- sistema/applications/pedidos/forms.py
+++ sistema/applications/pedidos/forms.py
@@ -16,3 +16,13 @@ class PedidosCreateForm(forms.ModelForm):
             'pedi_materiaprima',
             'pedi_insumos',
         )
+
+        widgets={
+            'ref_pedido':forms.NumberInput(attrs={'placeholder': 'Referencia del pedido'}),
+            'pedi_fecha':forms.DateInput(),
+            'pedi_comprobantePago':forms.TextInput(attrs={'placeholder':'Comprobante de Pago'}),
+            'pedi_proveedor':forms.Select(),
+            'pedi_materiaprima':forms.SelectMultiple(),
+            'pedi_insumos':forms.SelectMultiple(),
+
+        }
