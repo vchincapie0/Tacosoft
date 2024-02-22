55b5e5b8fd60f21ee873acb2a654d2ffe8f9758d
vivian_pc <carohinca1997@gmail.com>
2024-02-22 16:18:11 -0500

    Creacion updateform en pedidos

--- sistema/applications/pedidos/forms.py
+++ sistema/applications/pedidos/forms.py
@@ -38,3 +38,41 @@ class PedidosCreateForm(forms.ModelForm):
             'pedi_insumos':forms.SelectMultiple(),
 
         }
+
+class PedidosUpdateForm(forms.ModelForm):
+
+    PEDIDO_CHOICES=(
+        ('0','materia prima'),
+        ('1','insumos'),
+    )
+
+    tipo_pedido=forms.CharField(
+        label='Tipo de Pedido',
+        required=True,
+        choices=PEDIDO_CHOICES,
+        max_length=1,
+    )
+
+    class Meta:
+
+        model=Pedidos
+        fields=(
+            'ref_pedido',
+            'pedi_user',
+            'pedi_fecha',
+            'pedi_estado',
+            'pedi_comprobatePago',
+            'pedi_proveedor',
+            'pedi_materiaprima',
+            'pedi_insumos',
+        )
+
+        widgets={
+            'ref_pedido':forms.NumberInput(attrs={'placeholder': 'Referencia del pedido'}),
+            'pedi_fecha':forms.DateInput(),
+            'pedi_comprobatePago':forms.TextInput(attrs={'placeholder':'Comprobante de Pago'}),
+            'pedi_proveedor':forms.Select(),
+            'pedi_materiaprima':forms.SelectMultiple(),
+            'pedi_insumos':forms.SelectMultiple(),
+
+        }
