34eec9f1ecc187e8511d7b902c78f89ba5e29c85
vivian_pc <carohinca1997@gmail.com>
2024-02-22 16:22:19 -0500

    creacion listview pedidos con su respectivo html

--- sistema/applications/pedidos/views.py
+++ sistema/applications/pedidos/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+#Importacion de modelos y formularios
+from .models import Pedidos
+from .forms import PedidosCreateForm, PedidosUpdateForm
 
 # Create your views here.
+
+class PedidosListView(LoginRequiredMixin, ListView):
+    '''Clase para mostrar los datos de los Pedidos'''
+    model = Pedidos
+    template_name = "pedidos/list_pedidos.html"
+    login_url=reverse_lazy('users_app:login')
+    paginate_by=5
+    context_object_name = 'pedidos'
+
+    def get_queryset(self):
+        '''Funcion que toma de la barra de busqueda la pablabra clave para filtrar'''
+        palabra_clave= self.request.GET.get("kword",'')
+        lista = Pedidos.objects.filter(
+            ref_pedido__icontains = palabra_clave
+        )
+        return lista
+
