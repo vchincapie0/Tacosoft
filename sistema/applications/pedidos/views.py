1157cc635b70f42fba45670663d8f4e181e10223
vivian_pc <carohinca1997@gmail.com>
2024-02-23 15:31:50 -0500

    creacion views para PedidosAddProveedores

--- sistema/applications/pedidos/views.py
+++ sistema/applications/pedidos/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import (
+    TemplateView, 
+    ListView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView)
 from django.urls import reverse_lazy
+
 #Importacion de modelos y formularios
 from .models import Pedidos
-from .forms import PedidosCreateForm, PedidosUpdateForm, PedidosAddMpCreateFrom, PedidosAddInsumosCreateFrom
+from .forms import (
+    PedidosCreateForm, 
+    PedidosUpdateForm, 
+    PedidosAddMpCreateFrom, 
+    PedidosAddInsumosCreateFrom,
+    PedidosAddProveedorCreateFrom,
+)
+
 from applications.materiaprima.models import MateriaPrima
 from applications.insumos.models import ImplementosTrabajo
+from applications.proveedores.models import Proveedores
 
 # Create your views here.
 
@@ -48,11 +62,21 @@ class PedidosAddMpCreateView(LoginRequiredMixin,CreateView):
     success_url= reverse_lazy('pedidos_app:add_pedidos') 
 
 class PedidosAddInsumosCreateView(LoginRequiredMixin,CreateView):
-    '''Clase para crear una materia prima nueva dentro del formulario de agregar pedidos'''
+    '''Clase para crear un implemento de trabajo nuevo dentro del formulario de agregar pedidos'''
     model = ImplementosTrabajo
     template_name = "pedidos/add_it_pedidos.html"
     login_url=reverse_lazy('home_app:home')
     #Campos que se van a mostrar en el formulario
     form_class = PedidosAddInsumosCreateFrom
+    #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
+    success_url= reverse_lazy('pedidos_app:add_pedidos')
+
+class PedidosAddProveedoresCreateView(LoginRequiredMixin,CreateView):
+    '''Clase para crear un proveedor nuevo dentro del formulario de agregar pedidos'''
+    model = Proveedores
+    template_name = "pedidos/add_prov_pedidos.html"
+    login_url=reverse_lazy('home_app:home')
+    #Campos que se van a mostrar en el formulario
+    form_class = PedidosAddProveedorCreateFrom
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
     success_url= reverse_lazy('pedidos_app:add_pedidos') 
