cffd825c50ab0062a72f47c8711587e56b145388
vivian_pc <carohinca1997@gmail.com>
2024-02-23 16:17:52 -0500

    Se añade url para borrar pedido

--- sistema/applications/pedidos/urls.py
+++ sistema/applications/pedidos/urls.py
@@ -10,6 +10,10 @@ urlpatterns = [
     path('add_it_pedidos/',views.PedidosAddInsumosCreateView.as_view(),name='add__it_pedidos'),
     path('add_prov_pedidos/',views.PedidosAddProveedoresCreateView.as_view(),name='add__prov_pedidos'),
     path('update_pedidos/<pk>',views.PedidosUpdateView.as_view(),name='update_pedidos'),
+    path('delete_pedidos/<pk>',views.PedidosDeleteView.as_view(),name='delete_pedidos'),
+
+    
+    
 
 
 
