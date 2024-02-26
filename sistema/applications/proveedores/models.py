de577ea46a814cbbada01de1d4e9a2f178427d33
vivian_pc <carohinca1997@gmail.com>
2024-02-26 16:39:43 -0500

    Se comenta clase Proveedores

--- sistema/applications/proveedores/models.py
+++ sistema/applications/proveedores/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Proveedores(models.Model):
+    '''Clase para la creacion de tabla proveedores en bd'''
     prov_id=models.AutoField('id',primary_key=True)
     nit = models.IntegerField('NIT',unique=True)
     prov_nombre=models.CharField('Nombre',max_length=40)
