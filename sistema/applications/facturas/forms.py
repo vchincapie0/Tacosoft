a95bb1b24a06d21953099bd0e330853842996d8c
vivian_pc <carohinca1997@gmail.com>
2024-02-27 11:38:22 -0500

    se inicializa el formulario FacturaCreateForm

--- sistema/applications/facturas/forms.py
+++ sistema/applications/facturas/forms.py
@@ -1,2 +1,20 @@
 from django import forms
 from .models import Facturas
+
+class FacturaCreateForm(forms.ModelForm):
+    '''Formulario para crear facturas'''
+    class Meta:
+        model = Facturas
+        fields = (
+            'num_factura',
+            'fac_proveedor',
+            'fac_numeroPedido',
+            'fac_fechaLlegada',
+            'fac_numeroUnidades',
+            'fac_subtotal',
+            'fac_iva',
+            )
+        
+        widgets={
+            
+        }
