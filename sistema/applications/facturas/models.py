4165e546f6e6f95bfa79d46b0625a9c4d237766e
vivian_pc <carohinca1997@gmail.com>
2024-02-27 11:06:20 -0500

    Se termina de modificar clase factura antes de hacer migracion

--- sistema/applications/facturas/models.py
+++ sistema/applications/facturas/models.py
@@ -8,13 +8,18 @@ class Facturas(models.Model):
     '''Clase para la creacion de tabla factura en bd'''
 
     num_factura=models.PositiveIntegerField('Numero Factura',unique=True)
-    fac_proveedor=models.ForeignKey(Proveedores)
-    fac_numeroPedido=models.ForeignKey(Pedidos)
+    fac_proveedor=models.ForeignKey(Proveedores, on_delete=models.CASCADE)
+    fac_numeroPedido=models.ForeignKey(Pedidos,on_delete=models.CASCADE)
     fac_fechaLlegada=models.DateField('Fecha Llegada')
     fac_numeroUnidades=models.PositiveIntegerField('Numero de Unidades')
     fac_subtotal=models.FloatField('subtotal')
     fac_iva=models.FloatField('IVA')
-    fac_total=models.FloatField('Total')
     img_factura=models.ImageField(upload_to='facturas',blank=True,null=True)
 
-    
+    @property
+    def fac_total(self):
+        # Calcula el total sumando el subtotal y el IVA
+        return self.fac_subtotal + self.fac_iva
+
+    def __str__(self):
+        return f"{self.num_factura}-{self.fac_proveedor}-{self.fac_numeroPedido}-{self.fac_total}"
