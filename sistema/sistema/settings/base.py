2303dc4b9d5ec7378e06c9639cf0427917fde83e
vivian_pc <carohinca1997@gmail.com>
2024-02-27 10:45:39 -0500

    Se registra aplicacion facturas en base.py

--- sistema/sistema/settings/base.py
+++ sistema/sistema/settings/base.py
@@ -26,7 +26,9 @@ INSTALLED_APPS = [
     'applications.users',
     'applications.insumos',
     'applications.proveedores',
-    'applications.pedidos'
+    'applications.pedidos',
+    'applications.facturas',
+
 
 
 ]
