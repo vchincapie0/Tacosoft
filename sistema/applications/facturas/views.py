3ba9414828d635a131c965dbe89133ed593b4280
vivian_pc <carohinca1997@gmail.com>
2024-03-04 15:31:29 -0500

    Se inicia las vista de app facturas

--- sistema/applications/facturas/views.py
+++ sistema/applications/facturas/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+#Importacion de modelos y formularios
+from .models import Facturas
+#from .forms import 
 
 # Create your views here.
+
+class InsumosListView(LoginRequiredMixin, ListView):
+    '''Clase para mostrar los datos de los Implementos de trabajo'''
+    model = Facturas
+    template_name = "facturas/list_factura.html"
+    login_url=reverse_lazy('users_app:login')
+    paginate_by=10
+    context_object_name = 'facturas'
+
+    def get_queryset(self):
+        '''Funcion que toma de la barra de busqueda la pablabra clave para filtrar'''
+        palabra_clave= self.request.GET.get("kword",'')
+        lista = Facturas.objects.filter(
+            num_factura__icontains = palabra_clave
+        )
+        return lista
