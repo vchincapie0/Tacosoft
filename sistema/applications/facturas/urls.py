2e3fa40a987c9dbd19b5a486b1e30c4902574eff
Johan <jfreddytovar@gmail.com>
2024-03-05 14:22:18 -0500

    Se añade url de add_factura

--- sistema/applications/facturas/urls.py
+++ sistema/applications/facturas/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name='facturas_app'
 
 urlpatterns = [
-    path('list_facturas/', views.FacturasListView.as_view(), name='list_factura')
+    path('list_facturas/', views.FacturasListView.as_view(), name='list_factura'),
+    path('add_facturas/', views.FacturasCreateView.as_view(), name='add_factura'),
+
 ]
