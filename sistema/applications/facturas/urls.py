ff4f952ef4770430ab3d5c83ea80cd3fdf16e4bd
Kevin Albarracin <19kand@gmail.com>
2024-03-07 10:51:12 -0500

    create url Facturas update

--- sistema/applications/facturas/urls.py
+++ sistema/applications/facturas/urls.py
@@ -6,5 +6,7 @@ app_name='facturas_app'
 urlpatterns = [
     path('list_facturas/', views.FacturasListView.as_view(), name='list_factura'),
     path('add_facturas/', views.FacturasCreateView.as_view(), name='add_factura'),
+    path('edit_facturas/', views.FacturasUpdateView.as_view(), name='edit_factura'),
+
 
 ]
