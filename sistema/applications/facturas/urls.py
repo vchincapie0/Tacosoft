21cbb2e37ebca078166e4d3011932e36029ce509
Kevin Albarracin <19kand@gmail.com>
2024-03-07 10:54:52 -0500

    se añade url en botton list_factura

--- sistema/applications/facturas/urls.py
+++ sistema/applications/facturas/urls.py
@@ -6,7 +6,7 @@ app_name='facturas_app'
 urlpatterns = [
     path('list_facturas/', views.FacturasListView.as_view(), name='list_factura'),
     path('add_facturas/', views.FacturasCreateView.as_view(), name='add_factura'),
-    path('edit_facturas/', views.FacturasUpdateView.as_view(), name='edit_factura'),
+    path('edit_facturas/<pk>', views.FacturasUpdateView.as_view(), name='edit_factura'),
 
 
 ]
