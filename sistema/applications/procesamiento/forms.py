6405c6c9d4520340d3c8d669e9a72b42a0f52fb6
Kevin Albarracin <19kand@gmail.com>
2024-03-08 11:26:30 -0500

    create form procesamiento, update y add, con validación de errores

--- sistema/applications/procesamiento/forms.py
+++ sistema/applications/procesamiento/forms.py
@@ -26,39 +26,64 @@ class ProcesamientoForm(forms.ModelForm):
             'proces_pesoPostProceso':forms.NumberInput(attrs={'placeholder': 'Peso Post Proceso'}),
             'proces_merma':forms.NumberInput(attrs={'placeholder': 'Peso Merma'}),
             'proces_check':forms.Select(attrs={'placeholder': 'Estado de Chequeo'}),
-
-
-
             
         }
-
-    def clean_it_cantidad(self):
-        cantidad = self.cleaned_data['it_cantidad']
-        if cantidad <= 0:
-            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
-        return cantidad
-    
-    def clean_it_nombre(self):
-        nombre = self.cleaned_data['it_nombre']
+        
+    def clean_proces_nombreProd(self):
+        nombre = self.cleaned_data['proces_nombreProd']
         if not re.match("^[a-zA-Z ]+$", nombre):
             raise forms.ValidationError("El nombre no debe contener números o caracteres especiales.")
         return nombre
     
-class ImplementosUpdateForm(forms.ModelForm):
-    """Form Update Implementos de Trabajo."""
+
+    def clean_proces_pesoMpCruda(self):
+        cantidad = self.cleaned_data['proces_pesoMpCruda']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
+    
+    def clean_proces_pesoPostProceso(self):
+        cantidad = self.cleaned_data['proces_pesoPostProceso']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
+    
+    def clean_proces_merma(self):
+        cantidad = self.cleaned_data['proces_merma']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
+    
+    
+    
+class ProcesamientoUpdateForm(forms.ModelForm):
+    """Form Update Procesamiento de Trabajo."""
     class Meta:
 
-        model = ImplementosTrabajo
-        fields = ['it_nombre', 'it_cantidad', 'it_fechaEntrega', 'it_estado']
+        model =  Procesamiento 
+        fields = ['proces_nombreProd', 'proces_pesoMpCruda', 'proces_pesoPostProceso', 'proces_merma', 'proces_check']
     
-    def clean_it_cantidad(self):
-        cantidad = self.cleaned_data['it_cantidad']
+    def clean_proces_nombreProd(self):
+        nombre = self.cleaned_data['proces_nombreProd']
+        if not re.match("^[a-zA-Z ]+$", nombre):
+            raise forms.ValidationError("El nombre no debe contener números o caracteres especiales.")
+        return nombre
+    
+
+    def clean_proces_pesoMpCruda(self):
+        cantidad = self.cleaned_data['proces_pesoMpCruda']
         if cantidad <= 0:
             raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
         return cantidad
     
-    def clean_it_nombre(self):
-        nombre = self.cleaned_data['it_nombre']
-        if not re.match("^[a-zA-Z ]+$", nombre):
-            raise forms.ValidationError("El nombre no debe contener números o caracteres especiales.")
-        return nombre
+    def clean_proces_pesoPostProceso(self):
+        cantidad = self.cleaned_data['proces_pesoPostProceso']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
+    
+    def clean_proces_merma(self):
+        cantidad = self.cleaned_data['proces_merma']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
