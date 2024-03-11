2a06047050ae5c4618233b251073927560518c52
Daniel <castromercado1212@gmail.com>
2024-03-11 16:00:43 -0500

    cambios en urls y view

--- sistema/applications/materiaprima/views.py
+++ sistema/applications/materiaprima/views.py
@@ -96,11 +96,11 @@ class MateriaPrimaDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'materiaprima'
 
 
-class Existenciamp(LoginRequiredMixin):
+class ExistenciampDetailView(LoginRequiredMixin, DetailView):
     '''Vists para la creacion de la existencias materia prima'''
     model = Existenciamp
     template_name = "materiaprima/existencia_mp.html"
     login_url=reverse_lazy('users_app:login')
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
-    success_url= reverse_lazy('mp_app:exitencia_mp')
+    success_url= reverse_lazy('mp_app:detail_mp')
 
