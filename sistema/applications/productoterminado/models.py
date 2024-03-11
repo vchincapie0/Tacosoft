2d7576f9edb5ad5567624ce8ee3f5793d25ced17
Johan <jfreddytovar@gmail.com>
2024-03-11 16:58:30 -0500

    Se crea model de producto terminado

--- sistema/applications/productoterminado/models.py
+++ sistema/applications/productoterminado/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from applications.procesamiento.models import Coccion, Picado
 
-# Create your models here.
+# Creacion model Producto terminado.
 
 class ProductoTerminado(models.Model):
 
     PT_lote=models.AutoField('id',primary_key=True)
-    proces_nombreProd=models.CharField('Nombre', max_length=50)
-    proces_pesoMpCruda=models.FloatField('Peso Crudo')
-    proces_pesoPostProceso=models.FloatField('Peso Post Proceso')
-    proces_merma=models.FloatField('Peso Merma')
-    proces_check=models.CharField('estado',max_length=1, choices=CHECK_CHOICES)
+    ID=models.ForeignKey(Coccion,on_delete=models.CASCADE)
+    ID=models.ForeignKey(Picado,on_delete=models.CASCADE)
+    PT_prodNombre=models.CharField('Nombre Producto Terminado', max_length=50)
+    PT_fechapreparacion=models.DateField('Fecha Preparacion')
+    PT_fechavencimiento=models.DateField('Fecha Vencimiento')
 
-    proces_materiaprima=models.ManyToManyField(MateriaPrima, blank=True)
-    
+
 
     def __str__(self):
         return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
