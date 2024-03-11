5c95ddaf027a59859dfadc6d19d508e3bca04a7b
Johan <jfreddytovar@gmail.com>
2024-03-11 15:41:38 -0500

    se crea modal de equipos en procesamiento

--- sistema/applications/procesamiento/models.py
+++ sistema/applications/procesamiento/models.py
@@ -24,3 +24,23 @@ class Procesamiento(models.Model):
 
     def __str__(self):
         return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
+
+
+class Equipos(models.Model):
+
+    CHECK_CHOICES=(
+        ('0','Aprobado'),
+        ('1','No Aprobado'),
+
+    )
+
+    cod_procesamiento=models.ForeignKey(Procesamiento,on_delete=models.CASCADE)
+    equi_encargadoCocina=models.CharField('Nombre', max_length=50)
+    equi_encargadoEntrega=models.CharField('Nombre', max_length=50)
+    equi_calidad=models.CharField('estado',max_length=1, choices=CHECK_CHOICES)
+    equi_nombre=models.CharField('Nombre', max_length=50)
+    equi_check=models.CharField('estado',max_length=1, choices=CHECK_CHOICES)
+
+    
+    def __str__(self):
+        return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
