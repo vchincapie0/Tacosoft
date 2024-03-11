c6e6c9968bd5d2361e9190e2d3195239dc5d16ff
Johan <jfreddytovar@gmail.com>
2024-03-11 15:58:14 -0500

    se crea model Coccion en model procesamiento

--- sistema/applications/procesamiento/models.py
+++ sistema/applications/procesamiento/models.py
@@ -44,3 +44,20 @@ class Equipos(models.Model):
     
     def __str__(self):
         return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
+
+class Coccion(models.Model):
+
+    CHECK_CHOICES=(
+        ('0','Aprobado'),
+        ('1','No Aprobado'),
+
+    )
+
+    cod_procesamiento=models.ForeignKey(Procesamiento,on_delete=models.CASCADE)
+    cocc_tiempoCoccion=models.IntegerField(default=100)
+    cocc_temperaturafinal=models.FloatField('temperatura final')
+    cocc_check=models.CharField('estado',max_length=1, choices=CHECK_CHOICES)
+
+    
+    def __str__(self):
+        return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
