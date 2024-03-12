c1ce0dc1d7f68addbe6f1dad10bc07614ef70db0
Johan <jfreddytovar@gmail.com>
2024-03-12 13:05:25 -0500

    se crea model caract organo PT

--- sistema/applications/productoterminado/models.py
+++ sistema/applications/productoterminado/models.py
@@ -13,7 +13,7 @@ class ProductoTerminado(models.Model):
     PT_fechavencimiento=models.DateField('Fecha Vencimiento')
 
     def __str__(self):
-        return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
+        return f"{self.PT_lote}-{self.PT_prodNombre}-{self.PT_fechapreparacion}-{self.PT_fechavencimiento}"
 
 
 # Creación model ExistenciaPT.
@@ -26,5 +26,24 @@ class ExistenciaPT(models.Model):
 
 
     def __str__(self):
-        return f"{self.cod_procesamiento}-{self.proces_nombreProd}-{self.proces_materiaprima}"
+        return f"{self.PT_lote}-{self.ExisPT_CantidadIngresada}-{self.ExisPT_CantidadEgresada}"
 
+# Creación model caracteristicas organolepticas PT.
+    
+class CaracteristicasOrganolepticasPT(models.Model):
+
+    ESTADO_CHOICES=(
+        ('0','Aprobado'),
+        ('1','No Aprobado'),
+    )
+
+    PT_lote=models.ForeignKey(ProductoTerminado,on_delete=models.CASCADE)
+    observaciones = models.CharField('Observaciones', max_length=50)
+    olor = models.BooleanField('Olor',default=False)
+    sabor = models.BooleanField('Sabor',default=False)
+    textura = models.BooleanField('Textura',default=False)
+    color = models.BooleanField('Color',default=False)
+    estado=models.CharField('Estado',max_length=1,choices=ESTADO_CHOICES)
+    
+    def __str__(self):
+        return str(self.PT_lote)+'-'+self.estado
