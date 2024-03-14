fff05d94fbf1a20eb5a4355891a2b1f68a41e21a
Johan <jfreddytovar@gmail.com>
2024-03-14 10:59:03 -0500

    se  crea url producto terminado

---
+++ sistema/applications/productoterminado/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name='produ_app'
+
+urlpatterns = [
+    path('list_produ/',views.ProduListView.as_view(),name='list_produ'),
+    path('add_produ/',views.ProduCreateView.as_view(),name='add_produ'),
+    path('update_produ/<pk>',views.ProduUpdateView.as_view(),name='update_produ'),
+    path('delete_produ/<pk>',views.ProduDeleteView.as_view(),name='delete_produ'),
+]
