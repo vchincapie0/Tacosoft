dbb42c1f6cf9bdc8ba5e2e3b151cbd06b4c477e3
vivian_pc <carohinca1997@gmail.com>
2024-03-14 20:13:10 -0500

    Se cambia el redirect de la funcion

--- sistema/applications/materiaprima/views.py
+++ sistema/applications/materiaprima/views.py
@@ -47,9 +47,24 @@ class CaracteristicasMateriaPrimaCreateView(LoginRequiredMixin, CreateView):
     form_class = CaracteristicasMPForm
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
     success_url= reverse_lazy('mp_app:lista_mp')
+
+    def caracteristicasMP(request):
+        '''funcio para validar el formulario'''
+        if request.method == 'POST':
+            formulario = CaracteristicasMPForm(request.POST)
+
+            if formulario.is_valid():
+
+                return redirect('MateriaPrimaListView')
+        else:
+        # El formulario está vacío, así que simplemente inicializa un formulario vacío
+            formulario = CaracteristicasMPForm()
+
+    # Renderiza la vista con el formulario
+        return render(request, 'caracteristicas_mp.html', {'formulario': formulario})
     
 class CaracteristicasMateriaPrimaUpdateView(LoginRequiredMixin, UpdateView):
-    '''Vista para la edición de las caracteristicas organolepticas de la materia prima'''
+    '''Vista para la edicion de las caracteristicas organolepticas de la materia prima'''
     model = CaracteristicasOrganolepticas
     template_name = "materiaprima/updateCaracteristicas_mp.html"
     login_url=reverse_lazy('users_app:login')
@@ -57,6 +72,21 @@ class CaracteristicasMateriaPrimaUpdateView(LoginRequiredMixin, UpdateView):
     form_class = CaracteristicasMPUpdateForm
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
     success_url= reverse_lazy('mp_app:lista_mp')
+
+    def caracteristicasMP(request):
+        '''funcio para validar el formulario'''
+        if request.method == 'POST':
+            formulario = CaracteristicasMPUpdateForm(request.POST)
+
+            if formulario.is_valid():
+
+                return redirect('MateriaPrimaListView')
+        else:
+        # El formulario está vacío, así que simplemente inicializa un formulario vacío
+            formulario = CaracteristicasMPUpdateForm()
+
+    # Renderiza la vista con el formulario
+        return render(request, 'updateCaracteristicas_mp.html', {'formulario': formulario})
     
 class DesinfeccionMateriaPrimaCreateView(LoginRequiredMixin, CreateView):
     '''Vists para la creacion de la desinfeccion de la materia prima'''
