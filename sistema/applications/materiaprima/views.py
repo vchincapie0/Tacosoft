d4e9890699d39c777dc7b50195a2bb18a30b139d
vivian_pc <carohinca1997@gmail.com>
2024-03-14 19:14:00 -0500

    Se modifican botones de updatecaracteristicas

--- sistema/applications/materiaprima/views.py
+++ sistema/applications/materiaprima/views.py
@@ -63,7 +63,7 @@ class CaracteristicasMateriaPrimaCreateView(LoginRequiredMixin, CreateView):
 
             if formulario.is_valid():
 
-                return redirect('MateriaPrimaDetailView')
+                return redirect('MateriaPrimaListView')
         else:
         # El formulario está vacío, así que simplemente inicializa un formulario vacío
             formulario = CaracteristicasMPForm()
@@ -80,21 +80,6 @@ class CaracteristicasMateriaPrimaUpdateView(LoginRequiredMixin, UpdateView):
     form_class = CaracteristicasMPUpdateForm
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
     success_url= reverse_lazy('mp_app:lista_mp')
-
-    def caracteristicasMP(request):
-        '''funcio para validar el formulario'''
-        if request.method == 'POST':
-            formulario = CaracteristicasMPUpdateForm(request.POST)
-
-            if formulario.is_valid():
-
-                return redirect('MateriaPrimaDetailView')
-        else:
-        # El formulario está vacío, así que simplemente inicializa un formulario vacío
-            formulario = CaracteristicasMPUpdateForm()
-
-    # Renderiza la vista con el formulario
-        return render(request, 'updateCaracteristicas_mp.html', {'formulario': formulario})
     
 class DesinfeccionMateriaPrimaCreateView(LoginRequiredMixin, CreateView):
     '''Vists para la creacion de la desinfeccion de la materia prima'''
