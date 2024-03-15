9537bdd988703d4948381aa7f5833ba373fefadd
vivian_pc <carohinca1997@gmail.com>
2024-03-14 21:15:49 -0500

    Se añaden condiciones y validacion de errores en add_mp

--- sistema/applications/materiaprima/forms.py
+++ sistema/applications/materiaprima/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils import timezone
 from .models import MateriaPrima, Desinfeccion, CaracteristicasOrganolepticas
 
 class MateriaPrimaForm(forms.ModelForm):
@@ -23,11 +24,32 @@ class MateriaPrimaForm(forms.ModelForm):
             'mp_fechavencimiento':forms.SelectDateWidget(),
         }
     
-    def clean_it_cantidad(self):
-        cantidad = self.cleaned_data['it_cantidad']
+    def clean_mp_cantidad(self):
+        cantidad = self.cleaned_data['mp_cantidad']
         if cantidad <= 0:
             raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
         return cantidad
+    
+    def clean_mp_fechallegada(self):
+        fecha_llegada = self.cleaned_data['mp_fechallegada']
+        fecha_actual = timezone.now().date()
+        tres_dias_atras = fecha_actual - timezone.timedelta(days=3)
+
+        # Comprueba si la fecha de llegada está dentro del rango permitido
+        if fecha_llegada > fecha_actual or fecha_llegada < tres_dias_atras:
+            raise forms.ValidationError('La fecha de llegada debe ser el día actual o hasta 3 días antes.')
+        
+        return fecha_llegada
+    
+    def clean_mp_fechavencimiento(self):
+        fecha_vencimiento = self.cleaned_data['mp_fechavencimiento']
+        fecha_actual = timezone.now().date()
+
+        # Comprueba si la fecha de vencimiento es anterior a la fecha actual
+        if fecha_vencimiento < fecha_actual:
+            raise forms.ValidationError('La fecha de vencimiento debe ser posterior a la fecha actual.')
+
+        return fecha_vencimiento
 
 class CaracteristicasMPForm(forms.ModelForm):
 
--- sistema/applications/materiaprima/views.py
+++ sistema/applications/materiaprima/views.py
@@ -36,7 +36,7 @@ class MateriaPrimaCreateView(LoginRequiredMixin, CreateView):
     #Campos que se van a mostrar en el formulario
     form_class = MateriaPrimaForm
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
-    success_url= reverse_lazy('mp_app:lista_mp')    
+    success_url= reverse_lazy('mp_app:lista_mp') 
 
 class CaracteristicasMateriaPrimaCreateView(LoginRequiredMixin, CreateView):
     '''Vista para la creacion de las caracteristicas organolepticas de la materia prima'''
