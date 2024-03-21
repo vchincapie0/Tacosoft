42674fabee6663c10b88f45bb63760643f7a48f6
Daniel <castromercado1212@gmail.com>
2024-03-21 10:22:55 -0500

    creo url picado de procesamiento

--- sistema/applications/procesamiento/urls.py
+++ sistema/applications/procesamiento/urls.py
@@ -5,4 +5,5 @@ app_name='procesamientos_app'
 
 urlpatterns = [
     path('list_procesamientos/',views.ProcesamientosListView.as_view(),name='list_procesamientos'),
+    path('picado/',views.ProcesamientosListView.as_view(),name='picado'),
 ]
