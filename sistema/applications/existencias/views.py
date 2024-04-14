89ef0601a838e6a161a08ce764824c15b6d15a07
vivian_pc <carohinca1997@gmail.com>
2024-04-14 11:46:54 -0500

    Se crea view para la vista de stock desde base

--- sistema/applications/existencias/views.py
+++ sistema/applications/existencias/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class Existencias(LoginRequiredMixin,TemplateView):
+    template_name = "existencias/stock.html"
+    login_url=reverse_lazy('users_app:login')
+
