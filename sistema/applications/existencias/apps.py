e89d628fa42f65f76dc924979ced3668194a13cf
vivian_pc <carohinca1997@gmail.com>
2024-04-14 11:29:17 -0500

    Se modifica nombre

--- sistema/applications/existencias/apps.py
+++ sistema/applications/existencias/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ExistenciasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'existencias'
+    name = 'applications.existencias'
