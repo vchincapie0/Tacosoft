955cd92b5b651b1e74fa10e51d098f10e45450ab
Kevin Albarracin <19kand@gmail.com>
2024-04-15 16:43:08 -0500

    Se crea url desinfectante generico

--- sistema/applications/materiaprima/urls.py
+++ sistema/applications/materiaprima/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('mp_add/',views.MateriaPrimaCreateView.as_view(),name='add_mp'),
     path('mp_caracteristicas/',views.CaracteristicasMateriaPrimaCreateView.as_view(),name='caracteristicas_mp'),
     path('mp_update_caracteristicas/<pk>',views.CaracteristicasMateriaPrimaUpdateView.as_view(),name='updateCaracteristicas_mp'),
+    path('desinfeccion_generico/',views.DesinfectanteGenericoListView.as_view(),name='desinfeccion_generico'),
     path('mp_desinfeccion/',views.DesinfeccionMateriaPrimaCreateView.as_view(),name='desinfeccion_mp'),
     path('mp_update_desinfeccion/<pk>',views.DesinfeccionMateriaPrimaUpdateView.as_view(),name='updateDesinfeccion_mp'),
     path('mp_detail/<pk>',views.MateriaPrimaDetailView.as_view(),name='detail_mp'),
