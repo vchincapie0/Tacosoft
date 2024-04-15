356702231467ce2fd800366df24135fe793cee96
Kevin Albarracin <19kand@gmail.com>
2024-04-15 15:50:32 -0500

    se crea html insumos Generico

--- sistema/applications/insumos/urls.py
+++ sistema/applications/insumos/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name='insumos_app'
 
 urlpatterns = [
+    path('list_insumos_generico/',views.InsumosGenericoListView.as_view(),name='list_insumos_generico'),
     path('list_insumos/',views.InsumosListView.as_view(),name='list_insumos'),
     path('add_insumos/',views.ImplementosCreateView.as_view(),name='add_insumos'),
     path('update_insumos/<pk>',views.ImplementosUpdateView.as_view(),name='update_insumos'),
