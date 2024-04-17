2db295c81aacd97e5ea244206cd9d1e3647d7961
Kevin Albarracin <19kand@gmail.com>
2024-04-17 15:43:48 -0500

    se crea add_insumos_generico

--- sistema/applications/insumos/views.py
+++ sistema/applications/insumos/views.py
@@ -4,9 +4,10 @@ from django.views.generic import TemplateView, ListView, CreateView, UpdateView,
 from django.urls import reverse_lazy
 #Importacion de modelos y formularios
 from .models import ImplementosTrabajo,InsumosGenerico
-from .forms import ImplementosTrabajoForm, ImplementosUpdateForm
+from .forms import ImplementosTrabajoForm, ImplementosUpdateForm,InsumosGenericoForm
 
 # Create your views here.
+
 class InsumosGenericoListView(LoginRequiredMixin, ListView):
     '''Clase para mostrar los datos de los Implementos de trabajo'''
     model = InsumosGenerico
@@ -22,7 +23,16 @@ class InsumosGenericoListView(LoginRequiredMixin, ListView):
            it_nombre__icontains = palabra_clave
         )
         return lista
-    
+class InsumosGenericoCreateView(LoginRequiredMixin, CreateView):
+    '''Clase donde se crea una nueva materia prima'''
+    model = InsumosGenerico
+    template_name = "insumos/add_insumos_generico.html"
+    login_url=reverse_lazy('users_app:login')
+    #Campos que se van a mostrar en el formulario
+    form_class=InsumosGenericoForm
+    #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
+    success_url= reverse_lazy('insumos_app:list_insumos_generico')
+
 class InsumosListView(LoginRequiredMixin, ListView):
     '''Clase para mostrar los datos de los Implementos de trabajo'''
     model = ImplementosTrabajo
@@ -43,7 +53,7 @@ class ImplementosCreateView(LoginRequiredMixin, CreateView):
     '''Clase donde se crea una nueva materia prima'''
     model = ImplementosTrabajo
     template_name = "insumos/add_insumos.html"
-    login_url=reverse_lazy('home_app:home')
+    login_url=reverse_lazy('users_app:login')
     #Campos que se van a mostrar en el formulario
     form_class = ImplementosTrabajoForm
     #url donde se redirecciona una vez acaba el proceso el "." es para redireccionar a la misma pagina
