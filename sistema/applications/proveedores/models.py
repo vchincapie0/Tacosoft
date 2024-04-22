9c8c9c117cc631eba3af13400a1e29f907edfe67
vivian_pc <carohinca1997@gmail.com>
2024-04-22 16:56:52 -0500

    Se crea campo para borrado logico

--- sistema/applications/proveedores/models.py
+++ sistema/applications/proveedores/models.py
@@ -8,6 +8,12 @@ class Proveedores(models.Model):
     nit = models.IntegerField('NIT',unique=True)
     prov_nombre=models.CharField('Nombre',max_length=40)
     prov_telefono=models.CharField('Telefono',max_length=10)
+    is_active = models.BooleanField(default=True)  # Campo para el borrado lógico
 
     def __str__(self):
         return f"{self.prov_nombre}"
+    
+    def delete(self, using=None, keep_parents=False):
+        '''Funcion para borrado lógico'''
+        self.is_active = False  # Marcar como inactivo en lugar de eliminar
+        self.save(using=using)
