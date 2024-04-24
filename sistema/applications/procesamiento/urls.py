d9695039024f54296a226f21894cc2b1d74f9a93
Daniel <castromercado1212@gmail.com>
2024-04-24 14:20:04 -0500

    creo url de delete picado

--- sistema/applications/procesamiento/urls.py
+++ sistema/applications/procesamiento/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('list_procesamientos/',views.ProcesamientosListView.as_view(),name='list_procesamientos'),
     path('picado/',views.PicadoListView.as_view(),name='picado'),
     path('add_picado/',views.PicadocreateView.as_view(),name='add_picado'),
+    path('delete_picado/',views.PicadoDeleteView.as_view(),name='delete_picado'),
 ]
