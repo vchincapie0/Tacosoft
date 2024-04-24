61e00e5bcfc066b879fde01ee9e6361bbdc4cfc8
Daniel <castromercado1212@gmail.com>
2024-04-24 12:33:38 -0500

    correcciones en forms

--- sistema/applications/procesamiento/forms.py
+++ sistema/applications/procesamiento/forms.py
@@ -22,7 +22,7 @@ class addPicado(forms.ModelForm):
             )
         
         widgets={
-            
+            'cod_procesamiento':forms.NumberInput(attrs={'class':'form-control'}),
             'pica_nombre':forms.TextInput(attrs={'placeholder': 'Nombre del Producto'}),
             'pica_cantidad':forms.NumberInput(attrs={'placeholder': 'Peso '}),
             'pica_pesoMPposproceso':forms.NumberInput(attrs={'placeholder': 'Peso Post Proceso'}),
@@ -31,28 +31,28 @@ class addPicado(forms.ModelForm):
             
         }
         
-    #def clean_pica_nombre(self):
-       # nombre = self.cleaned_data['pica_nombre']
-        #if not re.match("^[a-zA-Z ]+$", nombre):
-            #raise forms.ValidationError("El nombre no debe contener números o caracteres especiales.")
-        #return nombre
+    def clean_pica_nombre(self):
+        nombre = self.cleaned_data['pica_nombre']
+        if not re.match("^[a-zA-Z ]+$", nombre):
+            raise forms.ValidationError("El nombre no debe contener números o caracteres especiales.")
+        return nombre
     
 
-    #def clean_pica_cantidad(self):
-        #cantidad = self.cleaned_data['pica_cantidad']
-        #if cantidad <= 0:
-            #raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
-        #return cantidad
+    def clean_pica_cantidad(self):
+        cantidad = self.cleaned_data['pica_cantidad']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
     
-    #def clean_proces_pesoPostProceso(self):
-        #cantidad = self.cleaned_data['pica_pesoMPposproceso']
-        #if cantidad <= 0:
-            #raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
-        #return cantidad
+    def clean_proces_pesoPostProceso(self):
+        cantidad = self.cleaned_data['pica_pesoMPposproceso']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
     
-    #def clean_pica_merma(self):
-        #cantidad = self.cleaned_data['pica_merma']
-        #if cantidad <= 0:
-            #raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
-        #return cantidad
+    def clean_pica_merma(self):
+        cantidad = self.cleaned_data['pica_merma']
+        if cantidad <= 0:
+            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
+        return cantidad
     
