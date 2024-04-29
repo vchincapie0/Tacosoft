39a28be85a2981d0d3000695ca4e58dab82c9920
vivian_pc <carohinca1997@gmail.com>
2024-04-29 16:56:11 -0500

    Se termina de modificar la señal de users para useraudit

--- sistema/applications/users/signals.py
+++ sistema/applications/users/signals.py
@@ -1,22 +1,28 @@
-from django.db.models.signals import post_save, pre_save
+import threading
+from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import User, UserAudit
+
+# Obtener el modelo de usuario personalizado
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def log_user_change(sender, instance, created, **kwargs):
+    current_user = getattr(threading, 'current_user', None)
+
+    if current_user:
+        changed_by = current_user
+
     if instance.deleted:
         action = 'D'  # Marcar como eliminado
-        details = f"Usuario {instance.username} marcado como borrado."
+        details = f"{instance.name} {instance.last_name} ha sido borrado."
     elif created:
         action = 'C'  # Creación de usuario
-        details = f"Usuario {instance.username} creado."
+        details = f"{instance.name} {instance.last_name} ha sido creado."
     else:
         action = 'U'  # Actualización de usuario
-        details = f"Usuario {instance.username} actualizado."
-        print("Se edita usuario")
+        details = f"La información de {instance.name} {instance.last_name} ha sido actualizado."
 
-    # Evitar crear UserAudit si no hay cambios significativos (solo en actualizaciones)
-    if not created or instance.deleted:
-        UserAudit.objects.create(user=instance, action=action, details=details)
-
-                
+    # Crear el registro de auditoría con el usuario que realizó la acción
+    UserAudit.objects.create(user=instance, action=action, details=details, changed_by=changed_by)
--- sistema/sistema/settings/base.py
+++ sistema/sistema/settings/base.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'applications.users.middleware.CurrentUserMiddleware',
 ]
 
 ROOT_URLCONF = 'sistema.urls'
