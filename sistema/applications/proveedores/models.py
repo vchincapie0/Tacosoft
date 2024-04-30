b709eef07060f5dc4d783a35bc5f75d038c6c5a5
vivian_pc <carohinca1997@gmail.com>
2024-04-29 20:32:12 -0500

    clean

--- sistema/applications/proveedores/models.py
+++ sistema/applications/proveedores/models.py
@@ -27,7 +27,7 @@ class ProveedoresAudit(models.Model):
     ]
 
     changed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    proveedor = models.ForeignKey(Proveedores,on_delete=models.CASCADE, related_name='audit_logs', default='None')
+    proveedor = models.ForeignKey(Proveedores,on_delete=models.CASCADE, related_name='audit_logs')
     action = models.CharField(max_length=1, choices=ACTION_CHOICES)
     details = models.TextField(blank=True, null=True)
     changed_at = models.DateTimeField(auto_now_add=True)
