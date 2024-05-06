e6cc3cd80ae071b187f119c71920e4c947eb9e32
Daniel <castromercado1212@gmail.com>
2024-05-06 13:21:05 -0500

    creo url de update_picado

--- sistema/applications/procesamiento/urls.py
+++ sistema/applications/procesamiento/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('picado/',views.PicadoListView.as_view(),name='picado'),
     path('add_picado/',views.PicadocreateView.as_view(),name='add_picado'),
     path('delete_picado/<pk>',views.PicadoDeleteView.as_view(),name='delete_picado'),
+    path('update_picado/<pk>',views.PicadoUpdateView.as_view(),name='update_picado'),
     path('coccion/',views.CoccionListView.as_view(),name='coccion'),
     path('add_coccion/',views.CoccioncreateView.as_view(),name='add_coccion'),
     path('delete_coccion/<pk>',views.CoccionDeleteView.as_view(),name='delete_coccion'),
