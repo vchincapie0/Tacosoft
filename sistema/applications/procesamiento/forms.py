af6c770fb93a3d2581b07f041c69dd104a434fee
Daniel <castromercado1212@gmail.com>
2024-05-12 13:03:13 -0500

    creo html de edit coccion

--- sistema/applications/procesamiento/forms.py
+++ sistema/applications/procesamiento/forms.py
@@ -66,6 +66,7 @@ class PicadoUpdateForm(forms.ModelForm):
             'pica_cantidad',
             'pica_pesoMPposproceso',
             'pica_merma'
+            
             ]
 
         widgets={
