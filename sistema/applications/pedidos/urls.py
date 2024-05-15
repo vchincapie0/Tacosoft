5316f0a12342228eec12889928ad160e127da692
vivian_pc <carohinca1997@gmail.com>
2024-05-15 12:21:55 -0500

    Se crea url para export_pedidos_to_excel

--- sistema/applications/pedidos/urls.py
+++ sistema/applications/pedidos/urls.py
@@ -5,6 +5,8 @@ app_name='pedidos_app'
 
 urlpatterns = [
     path('list_pedidos/',views.PedidosListView.as_view(),name='list_pedidos'),
+    path('pedidos/export/xls', views.export_pedidos_to_excel, name='export_pedidos_excel'),
+    #path('pedidos/export/csv', views.export_facturas_to_csv, name='export_pedidos_csv'),
     path('add_pedidos/',views.PedidosCreateView.as_view(),name='add_pedidos'),
     path('add_mp_pedidos/',views.PedidosAddMpCreateView.as_view(),name='add__mp_pedidos'),
     path('add_it_pedidos/',views.PedidosAddInsumosCreateView.as_view(),name='add__it_pedidos'),
