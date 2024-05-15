9d7ce0c5f302948e61f18c5089caed8053294cb7
vivian_pc <carohinca1997@gmail.com>
2024-05-15 17:10:55 -0500

    Se comienza a incluir permisos en proveedores_app

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from applications.users.decorators import admin_required
 from . import views
 
 app_name='proveedores_app'
 
 urlpatterns = [
-    path('list_proveedores/',views.ProveedoresListView.as_view(),name='list_proveedores'),
+    path('list_proveedores/',admin_required(views.ProveedoresListView.as_view()),name='list_proveedores'),
     path('proveedores/export/xls',views.export_proveedores_to_excel, name='export_proveedores_excel'),
     path('proveedores/export/cvs',views.export_proveedores_to_csv, name='export_proveedores_cvs'),
     path('add_proveedor/',views.ProveedoresCreateView.as_view(),name='add_proveedores'),
