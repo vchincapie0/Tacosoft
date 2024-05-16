f85de2f81bde865c2ac37223bdea0ee058ccc527
vivian_pc <carohinca1997@gmail.com>
2024-05-16 14:21:34 -0500

    Se añade permisos a url de proveedores

--- sistema/applications/proveedores/urls.py
+++ sistema/applications/proveedores/urls.py
@@ -6,11 +6,11 @@ app_name='proveedores_app'
 
 urlpatterns = [
     path('list_proveedores/',admin_required(views.ProveedoresListView.as_view()),name='list_proveedores'),
-    path('proveedores/export/xls',views.export_proveedores_to_excel, name='export_proveedores_excel'),
-    path('proveedores/export/cvs',views.export_proveedores_to_csv, name='export_proveedores_cvs'),
-    path('add_proveedor/',views.ProveedoresCreateView.as_view(),name='add_proveedores'),
-    path('edit_proveedor/<pk>',views.ProveedorUpdateView.as_view(),name='update_proveedores'),
-    path('delete_proveedor/<pk>',views.ProveedoresDeleteView.as_view(),name='delete_proveedores'),
-    path('proveedor_audit/',views.ProveedoresAuditListView.as_view(),name='proveedor_audit'),
+    path('proveedores/export/xls',admin_required(views.export_proveedores_to_excel), name='export_proveedores_excel'),
+    path('proveedores/export/cvs',admin_required(views.export_proveedores_to_csv), name='export_proveedores_cvs'),
+    path('add_proveedor/',admin_required(views.ProveedoresCreateView.as_view()),name='add_proveedores'),
+    path('edit_proveedor/<pk>',admin_required(views.ProveedorUpdateView.as_view()),name='update_proveedores'),
+    path('delete_proveedor/<pk>',admin_required(views.ProveedoresDeleteView.as_view()),name='delete_proveedores'),
+    path('proveedor_audit/',admin_required(views.ProveedoresAuditListView.as_view()),name='proveedor_audit'),
 
 ]
