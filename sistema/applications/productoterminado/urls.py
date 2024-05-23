a60b839691002f1bc03b5eef9a1fee799076b954
johan <jfreddytovar@gmail.com>
2024-05-23 15:06:52 -0500

    se elimina url de add y delete producto terminado

--- sistema/applications/productoterminado/urls.py
+++ sistema/applications/productoterminado/urls.py
@@ -6,9 +6,7 @@ app_name='produ_app'
 
 urlpatterns = [
     path('list_produ/',views.ProduListView.as_view(),name='list_produ'),
-    path('add_produ/',admin_required(views.ProduCreateView.as_view()),name='add_produ'),
     path('update_produ/<pk>',views.ProduUpdateView.as_view(),name='update_produ'),
-    path('delete_produ/<pk>',admin_required(views.ProduDeleteView.as_view()),name='delete_produ'),
     path('existenciaPT/<pk>' ,views.ExistenciaPTView.as_view(),name='exitenciaPT'),
     path('caracteristicas_PT/',views.CaracteristicasProductoTerminadoCreateView.as_view(),name='caracteristicas_PT'),
     path('updatecaracteristicas_pt/<pk>',views.CaracteristicasProductoTerminadoUpdateView.as_view(),name='updatecaracteristicas_pt'),
