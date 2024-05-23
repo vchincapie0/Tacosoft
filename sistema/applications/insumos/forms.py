83fe31a40cfdf7c5a85d8feacde91ee9e96c9d1b
johan <jfreddytovar@gmail.com>
2024-05-23 14:48:32 -0500

    se elimina form de create implementos de trabajo

--- sistema/applications/insumos/forms.py
+++ sistema/applications/insumos/forms.py
@@ -26,36 +26,6 @@ class InsumosGenericoUpdateForm(forms.ModelForm):
         widgets={
             'it_nombre':forms.TextInput(attrs={'class':'form-control'}),
             }
-        
-class ImplementosTrabajoForm(forms.ModelForm):
-
-    """Form definition Implementos de Trabajo."""
-
-    class Meta:
-        """Meta definition for implementosTrabajoform."""
-
-        model = ImplementosTrabajo
-        fields = (
-            'it_codigo',
-            'it_nombre',
-            'it_cantidad',
-            'it_fechaEntrega',
-            'it_estado',
-            )
-        
-        widgets={
-            'it_codigo':forms.NumberInput(attrs={'class':'form-control'}),
-            'it_nombre':forms.Select(attrs={'class':'form-select'}),
-            'it_cantidad':forms.NumberInput(attrs={'placeholder': 'Cantidad Entregada','class':'form-control'}),
-            'it_fechaEntrega':forms.SelectDateWidget(),
-            'it_estado':forms.Select(attrs={'class':'form-select'})
-        }
-
-    def clean_it_cantidad(self):
-        cantidad = self.cleaned_data['it_cantidad']
-        if cantidad <= 0:
-            raise forms.ValidationError("La cantidad debe ser un número mayor que 0.")
-        return cantidad
     
 class ImplementosUpdateForm(forms.ModelForm):
     """Form Update Implementos de Trabajo."""
