24ec76ffacb415e45183ffe9579c3587c2bb2f21
johan <jfreddytovar@gmail.com>
2024-05-23 15:57:54 -0500

    se cambia nombre de implementos a Insumos

--- sistema/applications/insumos/views.py
+++ sistema/applications/insumos/views.py
@@ -9,8 +9,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 #Importacion de modelos y formularios
-from .models import ImplementosTrabajo,InsumosGenerico,ImplementosAudit
-from .forms import  ImplementosUpdateForm,InsumosGenericoForm,InsumosGenericoUpdateForm,ImplementosAuditFilterForm
+from .models import Insumos,InsumosGenerico,InsumosAudit
+from .forms import  InsumosUpdateForm,InsumosGenericoForm,InsumosGenericoUpdateForm,InsumosAuditFilterForm
 
 # Create your views here.
 
@@ -49,34 +49,34 @@ class InsumosGenericoUpdateView(LoginRequiredMixin, UpdateView):
     success_url= reverse_lazy('insumos_app:list_insumos')
 
 class InsumosGenericoDeleteView(LoginRequiredMixin, DeleteView):
-    '''Vista para borrar Implenentos de Trabajo'''
+    '''Vista para borrar Insumos'''
     model = InsumosGenerico
     template_name = "administrador/genericas/delete_insumos_generico.html"
     login_url=reverse_lazy('users_app:login')
     success_url= reverse_lazy('insumos_app:list_insumos_generico')
 
 class InsumosListView(LoginRequiredMixin, ListView):
-    '''Clase para mostrar los datos de los Implementos de trabajo'''
-    model = ImplementosTrabajo
+    '''Clase para mostrar los datos de los Insumos'''
+    model = Insumos
     template_name = "insumos/list_insumos.html"
     login_url=reverse_lazy('users_app:login')
     paginate_by=10
     context_object_name = 'insumos'
 
     def get_queryset(self):
-        '''Funcion que toma de la barra de busqueda la pablabra clave para filtrar'''
+        '''Funcion que toma de la barra de busqueda la palabra clave para filtrar'''
         palabra_clave= self.request.GET.get("kword",'')
-        lista = ImplementosTrabajo.objects.filter(
+        lista = Insumos.objects.filter(
            it_nombre__it_nombre__icontains = palabra_clave
         )
         return lista
     
-class ImplementosUpdateView(LoginRequiredMixin, UpdateView):
+class InsumosUpdateView(LoginRequiredMixin, UpdateView):
     '''Vista para actualizar los datos de user'''
-    model = ImplementosTrabajo
+    model = Insumos
     template_name = "insumos/update_insumos.html"
     login_url=reverse_lazy('users_app:login')
-    form_class=ImplementosUpdateForm
+    form_class=InsumosUpdateForm
     success_url= reverse_lazy('insumos_app:list_insumos')
 
     def form_valid(self, form):
@@ -84,20 +84,20 @@ class ImplementosUpdateView(LoginRequiredMixin, UpdateView):
         nombre = form.cleaned_data['it_nombre']
 
         # Agregar un mensaje de éxito con el nombre de usuario
-        messages.success(self.request, f'¡El implemento de trabajo {nombre} se ha actualizado correctamente!')
+        messages.success(self.request, f'¡El insumo {nombre} se ha actualizado correctamente!')
 
-        return super(ImplementosUpdateView, self).form_valid(form)
+        return super(InsumosUpdateView, self).form_valid(form)
     
-class ImplementosDeleteView(LoginRequiredMixin, DeleteView):
-    '''Vista para borrar Implenentos de Trabajo'''
-    model = ImplementosTrabajo
+class InsumosDeleteView(LoginRequiredMixin, DeleteView):
+    '''Vista para borrar Insumos'''
+    model = Insumos
     template_name = "insumos/delete_insumos.html"
     login_url=reverse_lazy('users_app:login')
     success_url= reverse_lazy('insumos_app:list_insumos')
 
-class ImplementosAuditListView(LoginRequiredMixin, ListView):
-    model= ImplementosAudit
-    template_name='administrador/auditorias/implementosaudit.html'
+class InsumosAuditListView(LoginRequiredMixin, ListView):
+    model= InsumosAudit
+    template_name='administrador/auditorias/insumosaudit.html'
     paginate_by=10
     context_object_name='auditoria'
 
@@ -105,19 +105,19 @@ class ImplementosAuditListView(LoginRequiredMixin, ListView):
         queryset = super().get_queryset()
 
         # Obtener los parámetros de filtrado del formulario
-        form = ImplementosAuditFilterForm(self.request.GET)
+        form = InsumosAuditFilterForm(self.request.GET)
 
         # Aplicar filtros si el formulario es válido
         if form.is_valid():
-            implementos = form.cleaned_data.get('implementos')
+            insumos = form.cleaned_data.get('insumos')
             action = form.cleaned_data.get('action')
             changed_by = form.cleaned_data.get('changed_by')
             start_date = form.cleaned_data.get('start_date')
             end_date = form.cleaned_data.get('end_date')
 
             # Filtrar por usuario, acción, usuario que realizó el cambio y rango de fechas
-            if implementos:
-                queryset = queryset.filter(implementos=implementos)
+            if insumos:
+                queryset = queryset.filter(insumos=insumos)
             if action:
                 queryset = queryset.filter(action=action)
             if changed_by:
@@ -133,21 +133,21 @@ class ImplementosAuditListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['filter_form'] = ImplementosAuditFilterForm(self.request.GET)
+        context['filter_form'] = InsumosAuditFilterForm(self.request.GET)
         return context
 
-def export_implementos_to_excel(request):
+def export_insumos_to_excel(request):
     '''Vista para exportar datos de tabla insumos en formato excel'''
     # Obtener la fecha y hora actual
     fecha_descarga = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Obtener los datos de implementos que quieres exportar
-    implementos = ImplementosTrabajo.objects.filter(deleted=False)  # Filtrar proveedores activos
+    # Obtener los datos de insumos que quieres exportar
+    insumos = Insumos.objects.filter(deleted=False)  # Filtrar insumos activos
 
     # Crear un nuevo libro de Excel y una hoja de trabajo
     workbook = Workbook()
     worksheet = workbook.active
-    worksheet.title = 'Implementos'
+    worksheet.title = 'Insumos'
 
     # Establecer estilos para la primera línea (encabezado personalizado)
     title_font = Font(bold=True)
@@ -170,7 +170,7 @@ def export_implementos_to_excel(request):
     worksheet.append([])  # Agregar una fila vacía
 
     # Agregar encabezados a la siguiente fila
-    headers = ['Implemento', 'Cantidad', 'Fecha de Entrega','Estado']
+    headers = ['Insumo', 'Cantidad', 'Fecha de Entrega','Estado']
     worksheet.append(headers)
 
     # Aplicar estilos a la fila de encabezados (fila actual + 2)
@@ -185,12 +185,12 @@ def export_implementos_to_excel(request):
         header_cell.alignment = header_alignment
 
     # Agregar datos de implementos a las siguientes filas
-    for implemento in implementos:
+    for insumo in insumos:
         data_row = [
-            implemento.it_nombre.it_nombre,  # Access related field's name
-            implemento.it_cantidad,
-            implemento.it_fechaEntrega.strftime("%Y-%m-%d"),  # Format date
-            implemento.get_it_estado_display()  # Get display value of choice field
+            insumo.it_nombre.it_nombre,  # Access related field's name
+            insumo.it_cantidad,
+            insumo.it_fechaEntrega.strftime("%Y-%m-%d"),  # Format date
+            insumo.get_it_estado_display()  # Get display value of choice field
         ]
         worksheet.append(data_row)
 
@@ -203,21 +203,21 @@ def export_implementos_to_excel(request):
 
     return response
 
-def export_implementos_to_csv(request):
-    '''Vista para exportar datos de tabla implementos en formato CSV'''
-    implementos = ImplementosTrabajo.objects.filter(deleted=False)  # Obtener datos de proveedores
+def export_insumos_to_csv(request):
+    '''Vista para exportar datos de tabla insumos en formato CSV'''
+    insumos = Insumos.objects.filter(deleted=False)  # Obtener datos de proveedores
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename=insumos.csv'
 
     writer = csv.writer(response)
-    writer.writerow(['Implemento', 'Cantidad', 'Fecha de Entrega','Estado'])  # Encabezados de columnas
+    writer.writerow(['Insumo', 'Cantidad', 'Fecha de Entrega','Estado'])  # Encabezados de columnas
 
-    for implemento in implementos:
+    for insumo in insumos:
         writer.writerow([
-            implemento.it_nombre.it_nombre,  # Access related field's name
-            implemento.it_cantidad,
-            implemento.it_fechaEntrega.strftime("%Y-%m-%d"),  # Format date
-            implemento.get_it_estado_display()  # Get display value of choice field
+            insumo.it_nombre.it_nombre,  # Access related field's name
+            insumo.it_cantidad,
+            insumo.it_fechaEntrega.strftime("%Y-%m-%d"),  # Format date
+            insumo.get_it_estado_display()  # Get display value of choice field
         ])
 
 
